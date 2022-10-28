81ead34cd4b01eb1bc8bd2e1e62a88c063ebc06f
Luan Ferreira <luanramosferr@outlook.com>
2022-10-28 20:50:07 -0300

    mensal para anual

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const numberCards = document.querySelectorAll(".number-card")
+const button = document.querySelector("#chk")
 
 function clicou() {
-  numberCards.forEach(card => {
-    const cardInnerText = card.innerText.replace(',', '.')
-    let teste = cardInnerText.split(' ')[1]
-    teste = parseFloat(teste) // transforma em quebrado
-    teste = teste * 11
-    card.innerText = teste.toFixed(2)
-  });
-}
+    numberCards.forEach(card => {
+        let cardInnerText = card.innerText.replace(',', '.')
+        let teste = cardInnerText.split(' ')[1]
+        teste = parseFloat(teste) // transforma em quebrado
+        teste = teste * 10
+        card.innerText = teste.toFixed(2)
+        card.innerText = card.innerText.replace('.', ',')
+        card.innerText = `R$ ${card.innerText}`
+      });
+    }
