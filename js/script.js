a460dfccdfad151035058841f834953b509fb1b3
Luan Ferreira <luanramosferr@outlook.com>
2022-10-29 15:00:40 -0300

    clear code js

--- js/script.js
+++ js/script.js
@@ -2,24 +2,25 @@ const numberCards = document.querySelectorAll(".number-card")
 
 function isChecked() {
   if (document.getElementById("chk").checked){
-    numberCards.forEach(card => {
-      let cardInnerText = card.innerText.replace(',', '.')
-      let teste = cardInnerText.split(' ')[1]
-      teste = parseFloat(teste) // transforma em quebrado
-      teste = teste * 10
-      card.innerText = teste.toFixed(2)
-      card.innerText = card.innerText.replace('.', ',')
-      card.innerText = `R$ ${card.innerText}`
-    });
+    handleNumber('anual')
   } else {
-    numberCards.forEach(card => {
-      let cardInnerText = card.innerText.replace(',', '.')
-      let teste = cardInnerText.split(' ')[1]
-      teste = parseFloat(teste) // transforma em quebrado
-      teste = teste / 10
-      card.innerText = teste.toFixed(2)
-      card.innerText = card.innerText.replace('.', ',')
-      card.innerText = `R$ ${card.innerText}`
-    });
+    handleNumber('mensal')
   }
+}
+
+function handleNumber(plano) {
+  numberCards.forEach(card => {
+    let cardInnerText = card.innerText.replace(',', '.')
+    let numberPricing = cardInnerText.split(' ')[1]
+    numberPricing = parseFloat(numberPricing)
+    
+    if (plano === 'mensal') {
+      numberPricing /= 10 
+    } else if (plano === 'anual') {
+      numberPricing *= 10
+    }
+    card.innerText = numberPricing.toFixed(2)
+    card.innerText = card.innerText.replace('.', ',')
+    card.innerText = `R$ ${card.innerText}`
+  });
 }
