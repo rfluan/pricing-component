56a709292068dcc69720fd743338ee339bfbf807
Luan Ferreira <luanramosferr@outlook.com>
2022-10-28 21:16:39 -0300

    js finalizado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 const numberCards = document.querySelectorAll(".number-card")
 
-function clicou() {
+function isChecked() {
+  if (document.getElementById("chk").checked){
     numberCards.forEach(card => {
-        let cardInnerText = card.innerText.replace(',', '.')
-        let teste = cardInnerText.split(' ')[1]
-        teste = parseFloat(teste) // transforma em quebrado
-        teste = teste * 10
-        card.innerText = teste.toFixed(2)
-        card.innerText = card.innerText.replace('.', ',')
-        card.innerText = `R$ ${card.innerText}`
-      });
-    }
+      let cardInnerText = card.innerText.replace(',', '.')
+      let teste = cardInnerText.split(' ')[1]
+      teste = parseFloat(teste) // transforma em quebrado
+      teste = teste * 10
+      card.innerText = teste.toFixed(2)
+      card.innerText = card.innerText.replace('.', ',')
+      card.innerText = `R$ ${card.innerText}`
+    });
+  } else {
+    numberCards.forEach(card => {
+      let cardInnerText = card.innerText.replace(',', '.')
+      let teste = cardInnerText.split(' ')[1]
+      teste = parseFloat(teste) // transforma em quebrado
+      teste = teste / 10
+      card.innerText = teste.toFixed(2)
+      card.innerText = card.innerText.replace('.', ',')
+      card.innerText = `R$ ${card.innerText}`
+    });
+  }
+
+}
